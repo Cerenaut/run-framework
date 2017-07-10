68c887909493743258bd763135c82cea308d381e
Abdelrahman Ahmed <abdelm.is@gmail.com>
2017-07-10 20:34:28 +1000

    update run-framework.py and fix a couple of pylint warnings

--- scripts/run-framework/run-framework.py
+++ scripts/run-framework/run-framework.py
@@ -12,7 +12,7 @@ from agief_experiment.experiment import Experiment
 from agief_experiment.launchmode import LaunchMode
 from agief_experiment import utils
 
-help_generic = """
+HELP_GENERIC = """
 run-framework.py allows you to run each step of the AGIEF (AGI Experimental Framework), locally and on AWS.
 Each step can be toggled with a parameter prefixed with 'step'. See parameter list for description of parameters.
 As with all scripts that are part of AGIEF, the environment variables in VARIABLES_FILES are used.
@@ -30,44 +30,51 @@ Assumptions:
 
 
 def setup_arg_parsing():
+    """
+    Parse the commandline arguments
+    """
     import argparse
     from argparse import RawTextHelpFormatter
 
-    parser = argparse.ArgumentParser(description=help_generic, formatter_class=RawTextHelpFormatter)
+    parser = argparse.ArgumentParser(description=HELP_GENERIC, formatter_class=RawTextHelpFormatter)
 
     # generate input files from the java experiment description
     parser.add_argument('--step_gen_input', dest='main_class', required=False,
                         help='Generate input files for experiments, then exit. '
-                             'The value is the Main class to run, that defines the experiment, '
-                             'before exporting the experimental input files entities.json and data.json. ')
+                             'The value is the Main class to run, that defines '
+                             'the experiment, before exporting the experimental '
+                             'input files entities.json and data.json. ')
 
     # main program flow
     parser.add_argument('--step_remote', dest='remote_type',
-                        help='Run Compute on remote machine. This parameter can specify "simple" or "aws". '
+                        help='Run Compute on remote machine. This parameter can '
+                             'specify "simple" or "aws". '
                              'If "AWS", then launch remote machine on AWS '
                              '(then --instanceid or --amiid needs to be specified).'
                              'Requires setting key path with --ssh_keypath'
                              '(default= % (default)s)')
     parser.add_argument('--exps_file', dest='exps_file', required=False,
-                        help='Run experiments, defined in the file that is set with this parameter.'
-                             'Filename is within AGI_RUN_HOME that defines the '
-                             'experiments to run (with parameter sweeps) in json format (default=%(default)s).')
+                        help='Run experiments, defined in the file that is set '
+                             'with this parameter. Filename is within '
+                             'AGI_RUN_HOME that defines the experiments to run '
+                             '(with parameter sweeps) in json format (default=%(default)s).')
     parser.add_argument('--step_sync', dest='sync', action='store_true',
                         help='Sync the code and run folder. Copy from local machine to remote. '
                              'Requires setting --step_remote and key path with --ssh_keypath')
     parser.add_argument('--step_sync_s3_prefix', dest='sync_s3_prefix', required=False,
-                        help='Sync output files. Download relevant output '
-                             'files from a previous phase determined by prefix, to the remote machine.'
+                        help='Sync output files. Download relevant output files '
+                             'from a previous phase determined by prefix, to the remote machine.'
                              'Requires setting --step_remote and key path with --ssh_keypath')
     parser.add_argument('--step_compute', dest='launch_compute', action='store_true',
                         help='Launch the Compute node.')
     parser.add_argument('--step_shutdown', dest='shutdown', action='store_true',
-                        help='Shutdown instances and Compute (if --launch_per_session) after other stages.')
+                        help='Shutdown instances and Compute '
+                             '(if --launch_per_session) after other stages.')
     parser.add_argument('--step_export', dest='export', action='store_true',
                         help='Export entity tree and data at the end of each experiment.')
     parser.add_argument('--step_export_compute', dest='export_compute', action='store_true',
-                        help='Compute should export entity tree and data at the end of each experiment '
-                             '- i.e. saved on the Compute node.')
+                        help='Compute should export entity tree and data at '
+                             'the end of each experiment - i.e. saved on the Compute node.')
     parser.add_argument('--step_upload', dest='upload', action='store_true',
                         help='Upload exported entity tree and data at the end of each experiment.')
     parser.add_argument('--DEBUG-NO-RUN', dest='debug_no_run', action='store_true',
@@ -76,37 +83,40 @@ def setup_arg_parsing():
     # how to reach the Compute node
     parser.add_argument('--host', dest='host', required=False,
                         help='Host where the Compute node will be running (default=%(default)s). '
-                             'THIS IS IGNORED IF RUNNING ON AWS (in which case the IP of the instance '
-                             'specified by the Instance ID is used)')
+                             'THIS IS IGNORED IF RUNNING ON AWS (in which case '
+                             'the IP of the instance specified by the Instance ID is used)')
     parser.add_argument('--port', dest='port', required=False,
                         help='Port where the Compute node will be running (default=%(default)s).')
     parser.add_argument('--user', dest='user', required=False,
-                        help='If remote, the "user" on the remote Compute node (default=%(default)s).')
+                        help='If remote, the "user" on the remote '
+                             'Compute node (default=%(default)s).')
     parser.add_argument('--ssh_port', dest='ssh_port', required=False,
-                        help='Which port to use for ssh when communicating with remote machine (default=%(default)s).')
+                        help='Which port to use for ssh when communicating '
+                             'with remote machine (default=%(default)s).')
     parser.add_argument('--remote_variables_file', dest='remote_variables_file', required=False,
-                        help='If remote, the path to the remote VARIABLES_FILE to use on the remote Compute node '
-                             '(default=%(default)s).')
+                        help='If remote, the path to the remote VARIABLES_FILE '
+                             'to use on the remote Compute node (default=%(default)s).')
 
     # launch mode
     parser.add_argument('--launch_per_session', dest='launch_per_session', action='store_true',
-                        help='Compute node is launched once at the start (and shutdown at the end if you use '
+                        help='Compute node is launched once at the start '
+                             '(and shutdown at the end if you use '
                              '--step_shutdown. Otherwise, it is launched and shut per experiment.')
 
     parser.add_argument('--no_docker', dest='no_docker', action='store_true',
-                        help='If set, then DO NOT launch in a docker container. Applies to LOCAL usage only. '
-                             '(default=%(default)s). ')
+                        help='If set, then DO NOT launch in a docker container. '
+                             'Applies to LOCAL usage only. (default=%(default)s). ')
 
     # aws/remote details
     parser.add_argument('--instanceid', dest='instanceid', required=False,
-                        help='Instance ID of the ec2 container instance - to start an ec2 instance, use this OR ami id,'
-                             ' not both.')
+                        help='Instance ID of the ec2 container instance - to '
+                             'start an ec2 instance, use this OR ami id, not both.')
     parser.add_argument('--amiid', dest='amiid', required=False,
-                        help='AMI ID for new ec2 instances - to start an ec2 instance, use this OR instance id, not'
-                             ' both.')
+                        help='AMI ID for new ec2 instances - to start an ec2 '
+                             'instance, use this OR instance id, not both.')
     parser.add_argument('--ami_ram', dest='ami_ram', required=False,
-                        help='If launching ec2 via AMI, use this to specify how much minimum RAM you want '
-                             '(default=%(default)s).')
+                        help='If launching ec2 via AMI, use this to specify '
+                             'how much minimum RAM you want (default=%(default)s).')
     parser.add_argument('--task_name', dest='task_name', required=False,
                         help='The name of the ecs task (default=%(default)s).')
     parser.add_argument('--ssh_keypath', dest='ssh_keypath', required=False,
@@ -114,8 +124,10 @@ def setup_arg_parsing():
                              'used for comms over ssh (default=%(default)s).')
     parser.add_argument('--pg_instance', dest='pg_instance', required=False,
                         help='Instance ID of the Postgres ec2 instance (default=%(default)s). '
-                             'If you want to use a running postgres instance, just specify the host (e.g. localhost). '
-                             'WARNING: assumes that if the string starts with "i-", then it is an Instance ID')
+                             'If you want to use a running postgres instance, '
+                             'just specify the host (e.g. localhost). WARNING: '
+                             'assumes that if the string starts with "i-", '
+                             'then it is an Instance ID')
 
     parser.add_argument('--logging', dest='logging', action='store_true', help='Turn logging on.')
 
@@ -124,7 +136,8 @@ def setup_arg_parsing():
     parser.set_defaults(port="8491")
     parser.set_defaults(ssh_port="22")
     parser.set_defaults(user="ec2-user")
-    parser.set_defaults(remote_variables_file="/home/ec2-user/agief-project/variables/variables-ec2.sh")
+    parser.set_defaults(remote_variables_file="/home/ec2-user/agief-project/"
+                                              "variables/variables-ec2.sh")
     parser.set_defaults(pg_instance="localhost")
     parser.set_defaults(task_name="mnist-spatial-task:10")
     parser.set_defaults(ssh_keypath=utils.filepath_from_env_variable(".ssh/ecs-key.pem", "HOME"))
@@ -135,6 +148,12 @@ def setup_arg_parsing():
 
 
 def check_args(args, compute_node):
+    """
+    Checks the arguments and ensures the required parameters are set
+
+    :param args: The commandline arguments
+    :param compute_node: The instance of the Compute class
+    """
     if args.amiid and args.instanceid:
         logging.error("Both the AMI ID and EC2 Instance ID have been specified."
                       " Use just one to specify how to get a running ec2 instance")
@@ -146,8 +165,8 @@ def check_args(args, compute_node):
         exit(1)
 
     if args.ssh_keypath and not compute_node.remote():
-        logging.warn("A keypath has been set, but we're not running on a "
-                     "remote machine (arg: step_remote). It will have no effect.")
+        logging.warning("A keypath has been set, but we're not running on a "
+                        "remote machine (arg: step_remote). It will have no effect.")
 
     if args.sync and not compute_node.remote():
         logging.error("Syncing experiment is meaningless unless you're running "
@@ -155,16 +174,16 @@ def check_args(args, compute_node):
         exit(1)
 
     if args.exps_file and not args.launch_compute:
-        logging.warn("You have elected to run experiment without launching a "
-                     "Compute node. For success, you'll have to have one "
-                     "running already, or use param --step_compute)")
+        logging.warning("You have elected to run experiment without launching a "
+                        "Compute node. For success, you'll have to have one "
+                        "running already, or use param --step_compute)")
 
 
+# pylint: disable=R0915, R0912, R0914
 def main():
-
-    # setup logging
-    log_format = "[%(filename)s:%(lineno)s - %(funcName)s() - %(levelname)s] %(message)s"
-    logging.basicConfig(format=log_format)
+    """
+    The main scope of the run-framework containing the high level code
+    """
 
     print("------------------------------------------")
     print("----          run-framework           ----")
@@ -174,12 +193,16 @@ def main():
     exp_start_time = datetime.now()
 
     args = setup_arg_parsing()
-    if args.logging:
-        logging.debug("Arguments: ", args)
-        logging.debug ("Python Version: " + sys.version)
+
+    # setup logging
+    log_format = "[%(filename)s:%(lineno)s - %(funcName)s() - %(levelname)s] %(message)s"
+    logging.basicConfig(format=log_format, level=utils.logger_level(args.logging))
+
+    logging.debug("Python Version: " + sys.version)
+    logging.debug("Arguments: %s", args)
 
     exps_file = args.exps_file if args.exps_file else ""
-    experiment = Experiment(args.logging, args.debug_no_run, LaunchMode.from_args(args), exps_file)
+    experiment = Experiment(args.debug_no_run, LaunchMode.from_args(args), exps_file)
 
     # 1) Generate input files
     if args.main_class:
@@ -191,14 +214,16 @@ def main():
 
     # *) all other use cases (non Generate input files)
 
-    cloud = Cloud(args.logging)
+    cloud = Cloud()
 
     if args.upload and not (args.export or args.export_compute):
-        logging.warning("Uploading experiment to S3 is enabled, but 'export experiment' is not, so the most "
-                        "important files (output entity.json and data.json) will be missing")
+        logging.warning("Uploading experiment to S3 is enabled, but "
+                        "'export experiment' is not, so the most important "
+                        "files (output entity.json and data.json) will be missing")
 
     if args.remote_type != "local":
-        host_node = HostNode(args.host, args.user, args.ssh_keypath, args.remote_variables_file, args.ssh_port)
+        host_node = HostNode(args.host, args.user, args.ssh_keypath,
+                             args.remote_variables_file, args.ssh_port)
     else:
         host_node = HostNode(args.host, args.user)
 
@@ -218,7 +243,9 @@ def main():
             ips = cloud.ec2_start_from_instanceid(args.instanceid)
             instance_id = args.instanceid
         else:
-            ips, instance_id = cloud.ec2_start_from_ami('run-fwk auto', args.amiid, int(args.ami_ram))
+            ips, instance_id = cloud.ec2_start_from_ami('run-fwk auto',
+                                                        args.amiid,
+                                                        int(args.ami_ram))
 
         # start DB ec2, from instanceid
         if args.pg_instance and is_pg_ec2:
@@ -228,7 +255,8 @@ def main():
 
     elif args.pg_instance:
         if is_pg_ec2:
-            logging.error("The pg instance is set to an ec2 instance id, but you are not running AWS.")
+            logging.error("The pg instance is set to an ec2 instance id,"
+                          " but you are not running AWS.")
             exit(1)
 
         ips_pg = {'ip_public': args.pg_instance, 'ip_private': args.pg_instance}
@@ -249,31 +277,34 @@ def main():
         if args.sync:
             cloud.sync_experiment(compute_node.host_node)
 
-        # 3.5) Sync data from S3 (typically used to download output files from a prev. experiment to be used as input)
+        # 3.5) Sync data from S3 (typically used to download output files
+        # from a prev. experiment to be used as input)
         if args.sync_s3_prefix:
             cloud.remote_download_output(args.sync_s3_prefix, compute_node.host_node)
 
         # 4) Launch Compute (remote or local) - *** IF Mode == 'Per Session' ***
         if (LaunchMode.from_args(args) is LaunchMode.per_session) and args.launch_compute:
-            compute_node.launch(experiment, cloud=cloud, main_class=args.main_class, no_local_docker=args.no_docker)
+            compute_node.launch(experiment, cloud=cloud,
+                                main_class=args.main_class,
+                                no_local_docker=args.no_docker)
 
         # 5) Run experiments (includes per experiment 'export results' and 'upload results')
         if args.exps_file:
             experiment.run_sweeps(compute_node, cloud, args)
             experiment.persist_prefix_history()
 
-    except Exception as e:
+    except Exception as err: # pylint: disable=W0703
         failed = True
 
-        logging.error("Something failed running sweeps generally. "
-                      "If the error occurred in a specific parameter set it should have been caught there. "
+        logging.error("Something failed running sweeps generally. If the error "
+                      "occurred in a specific parameter set it should have been caught there. "
                       "Attempt to shut down infrastructure if running, and exit.")
-        logging.error(e)
+        logging.error(err)
 
         # Shutdown the Docker container
         print("Attempting to shutdown Docker container...")
         if host_node.remote() and compute_node.container_id:
-            utils.remote_run(host_node, 'docker stop ' + compute_node.container_id, True)
+            utils.remote_run(host_node, 'docker stop ' + compute_node.container_id)
         elif not host_node.remote() and not args.no_docker:
             utils.docker_stop()
 
