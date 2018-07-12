33b44dce103cf04922d6a68894f92f4820a4519d
Abdelrahman Ahmed <abdelm.is@gmail.com>
2018-07-12 17:04:41 +1000

    Move tf sync to new experiment class

--- scripts/run-framework/tf_experiment/experiment.py
+++ scripts/run-framework/tf_experiment/experiment.py
@@ -3,6 +3,15 @@
 class Experiment(object):
   """Base class for TensorFlow-based experiments."""
 
+  def sync_experiment(self, remote):
+    """
+    Sync experiment from this machine to remote machine
+    """
+    print('\n....... Sync Experiment')
+
+    cmd = '../remote/remote-sync-tf-experiment.sh ' + remote.host_key_user_variables()
+    utils.run_bashscript_repeat(cmd, 15, 6)
+
   def run_sweeps(self, config, args, host_node, hparams_sweeps):
     """Run the sweeps"""
     raise NotImplementedError('Not implemented')
